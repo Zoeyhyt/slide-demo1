afdb92ea7ec56b4f102153d44dde65cc1e8f2010
zoey <echohyt@sohu.com>
2018-01-15 13:25:13 +0800

    修改了一个bug

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 let n 
 initialize()
-setInterval(() =>{
+let timer = setInterval(() =>{
     makeLeave(getImage(n))
     .one('transitionend',(e) =>{
         makeEnter($(e.currentTarget))
     })
     makeCurrent(getImage(n+1))
     n+=1
-},3000)
+},2000)
 
+document.addEventListener('visibilitychange',function(){
+    if(document.hidden){
+        window.clearInterval(timer)
+    }else{
+        timer = setInterval(() =>{
+            makeLeave(getImage(n))
+            .one('transitionend',(e) =>{
+                makeEnter($(e.currentTarget))
+            })
+            makeCurrent(getImage(n+1))
+            n+=1
+        },2000)
+    }
+})
 
 
 function x(n){
